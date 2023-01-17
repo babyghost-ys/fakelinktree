9d3368399c88a71e6161599ab74d513235c977fe
Peter <babyghost@gmail.com>
2023-01-16 21:24:24 -0600

    Added simple code to load the avatar.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import data from '../data.json'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center">
+      <Image alt={data.name} src={data.avatar} width={130} height={130} />
     </div>
   )
 }
